ff5ceaee66a5fd948e18906bcaacffe5f37028a1
hoangnam1555 <58805511+hoangnam1555@users.noreply.github.com>
2019-12-18 22:20:51 +0700

    demo

--- Demo/src/main/java/demogit/DemoGit.java
+++ Demo/src/main/java/demogit/DemoGit.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main;
+package demogit;
 
 import Unit.MyToys;
 import Unit.MyToys;
@@ -14,7 +14,7 @@ import Unit.MyToys;
  *
  * @author USER
  */
-public class DemoGit2 {
+public class DemoGit {
 
     /**
      * @param args the command line arguments
