848eac89ce3833858847d69350f9ffbce5f5d3fb
hoangnam1555 <58805511+hoangnam1555@users.noreply.github.com>
2019-12-18 21:02:05 +0700

    demo

---
+++ DemoGit3/src/main/java/main/DemoGit2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Unit.MyToys;
+import Unit.MyToys;
+
+
+
+/**
+ *
+ * @author USER
+ */
+public class DemoGit2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("5! = " + MyToys.computeFactoria(5));
+        System.out.println("6! = " + MyToys.computeFactoria(6));
+        //vi dieu
+        System.out.println("-5! = " + MyToys.computeFactoria(-5));
+    }
+    
+}
